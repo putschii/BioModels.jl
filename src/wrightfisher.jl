154137b180dbe557008f2ecd2e5ff4374f050d6b
putschii <44374737+putschii@users.noreply.github.com>
2020-04-16 15:31:00 +0200

    Update wrightfisher.jl

--- src/wrightfisher.jl
+++ src/wrightfisher.jl
@@ -13,93 +13,105 @@ It calculates and returns the population until fixation for each time step. If s
        """
 function wrightfisher(Input::Array,Fittest, Mutationrate, Steps, Save::Bool, File)
 
-
+    
 ### Error for wrong input
+    
+    #if typeof(Fittest) != BioSequence{DNAAlphabet{4}}
+    #    throw("Fittest has to be a sequence")
+   # end
 
-
-
-
+    
+        
     if Mutationrate == NaN || typeof(Mutationrate) != Float64 || typeof(Mutationrate) != Int64 && typeof(Mutationrate) != Float64
         throw("Mutationrate has to be a float")
     end
-
+    
     if typeof(Save) != Bool
        throw("Dynamics has to be a Bool")
     end
-
+    
     if typeof(Steps) != Int64 && typeof(Steps) != Int32
         throw("Steps has to be a Int")
-    end
-
-
-
+    end 
+        
+  #  for i in 1:length(Input)
+  #      if typeof(Input[i]) != BioSequence{DNAAlphabet{4}}
+   #         throw("Input must contain an Array with BioSequence{DNAAlphabet{4}}")
+  #      end
+  #  end
+        
     if typeof(File) != String
         throw("File has to be a String")
     end
-
+    
 ### First Part
-
+    
     ## give starting pop
     pop = Matrix(undef,length(Input),length(Input[1]))
-
+    
     for i in 1:length(Input)
         for j in 1:length(Input[i])
             pop[i,j] = Input[i][j]
         end
     end
-
+    
     # Size of Pop
     poplength = []
 
     for i in 1:size(pop)[1]
         push!(poplength,i)
     end
-
-
+    
+    
     # Turn Fittest to Matrix
     fit = Matrix(undef,1,length(Fittest))
-
+    
     for i in 1:length(Fittest)
         fit[1,i] = Fittest[i]
     end
-
+    
 
     # Matrix for selection
     selmatrix = Matrix(undef,length(Input),length(Input[1]))
-
+    
     sel = Float64[]
+    
+    # matrix for fixation        
+    fixmatrix = Matrix(undef,length(Input),length(Input[1]))    
+    
 
-    # matrix for fixation
-    fixmatrix = Matrix(undef,length(Input),length(Input[1]))
-
-
-    # fitnesscounter
+    # fitnesscounter    
     fitnesscounter = 0
 
     # just a counter for loops
     counter = 1
-
+    
     # adding base for mutation
     basesA = [DNA_T,DNA_G,DNA_C]
     basesG = [DNA_A,DNA_T,DNA_C]
     basesT = [DNA_A,DNA_G,DNA_C]
     basesC = [DNA_A,DNA_T,DNA_G]
 
-
+    
     # number of mutations in simulation
     mutationsteps = 0
-
+    
     # timestep counter
-    timesteps = 1
-
+    timesteps = 0
+    
     # savecounter
     savecounter = 1
     filecounter = 1
 
-
+    if Save == true
+        f = jldopen("$File", "a+")
+        write(f, "0", pop)
+        close(f)
+    end
+    
     ### Second Part ###
-
-
+    
+    
     # calculate selection for elements in array Input
     for i in 1:size(pop)[1]
         for j in 1:length(pop[i,:])
@@ -110,15 +122,15 @@ function wrightfisher(Input::Array,Fittest, Mutationrate, Steps, Save::Bool, Fil
             end
         end
     end
-
+    
     for i in 1:size(selmatrix)[1]
         push!(sel,sum(selmatrix[i,:]))
     end
-
+    
     # give weight to selection coefficent
     sel = Weights(sel)
 
-    # Fixationmatrix with same bases (1 for missmatch, 0 for match)
+    # Fixationmatrix with same bases (1 for missmatch, 0 for match)        
     for i in 1:size(pop)[2]
         for j in 1:(length(pop[:,i])-1)
             if pop[:,i][j] == pop[:,i][j+1]
@@ -134,18 +146,19 @@ function wrightfisher(Input::Array,Fittest, Mutationrate, Steps, Save::Bool, Fil
                 end
             end
         end
-
+            
     end
-
+    
     finish = sum(fixmatrix)
-
+    
     ### Third part
-
-
+    
+    
     # loop for sampling until pop is one species
     while finish != 0 && timesteps <= Steps
-
+    
     # Populate
+        timesteps = timesteps + 1
         sizecounter = size(pop)[1]
         while counter <= sizecounter
             sampler = Matrix(undef,1,length(pop[1,:]))
@@ -156,33 +169,33 @@ function wrightfisher(Input::Array,Fittest, Mutationrate, Steps, Save::Bool, Fil
             pop = vcat(pop,sampler)
             counter = counter + 1
         end
-
+        
     # Counter reset
-        counter = 1
-
+        counter = 1 
+        
     # Remove old Population
         pop = pop[(length(Input)+1):size(pop)[1],:]
 
     # Mutate
         for i in 1:length(pop)
-
+            
             if pop[i] == DNA_A
                 if rand() <= Mutationrate
                     pop[i] = sample(basesA)
                     mutationsteps = mutationsteps + 1
-
+                        
                 end
             elseif pop[i] == DNA_G
                 if rand() <= Mutationrate
                     pop[i] = sample(basesG)
                     mutationsteps = mutationsteps + 1
-
+                        
                 end
             elseif pop[i] == DNA_C
                 if rand() <= Mutationrate
                     pop[i] = sample(basesC)
                     mutationsteps = mutationsteps + 1
-
+                        
                 end
             elseif pop[i] == DNA_T
                 if rand() <= Mutationrate
@@ -193,14 +206,14 @@ function wrightfisher(Input::Array,Fittest, Mutationrate, Steps, Save::Bool, Fil
         end
 
   ##########  # find new selection coefficent
-
+        
     # Reset selection
-
-        selmatrix = Matrix(undef,length(Input),length(Input[1]))
+        
+        selmatrix = Matrix(undef,length(Input),length(Input[1]))    
         sel = Float64[]
-        fixmatrix = Matrix(undef,length(Input),length(Input[1]))
-
-    # calculate selection
+        fixmatrix = Matrix(undef,length(Input),length(Input[1])) 
+      
+    # calculate selection 
 
         for i in 1:size(pop)[1]
             for j in 1:length(pop[i,:])
@@ -215,13 +228,13 @@ function wrightfisher(Input::Array,Fittest, Mutationrate, Steps, Save::Bool, Fil
         for i in 1:size(selmatrix)[1]
             append!(sel,sum(selmatrix[i,:]))
         end
-
+        
     # give weight to selection coefficent
         sel = Weights(sel)
-
+     
     # Fixationmatrix with same bases (1 for missmatch, 0 for match)
 
-
+        
         for i in 1:size(pop)[2]
             for j in 1:(length(pop[:,i])-1)
                 if pop[:,i][j] == pop[:,i][j+1]
@@ -238,7 +251,7 @@ function wrightfisher(Input::Array,Fittest, Mutationrate, Steps, Save::Bool, Fil
                 end
             end
         end
-
+    
 
         finish = sum(fixmatrix)
     # Save Matrix for every timestep
@@ -247,10 +260,9 @@ function wrightfisher(Input::Array,Fittest, Mutationrate, Steps, Save::Bool, Fil
                 write(f, "$timesteps", pop)
                 close(f)
         end
-    # increase timesteps
-        timesteps = timesteps + 1
-    end
 
+    end
+    
     if Save == true
         println("Data is saved in file")
     else
